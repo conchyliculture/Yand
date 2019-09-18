d4b64e96533939d13c460041d36381a35897952b
flashdumper <6418006+conchyliculture@users.noreply.github.com>
2019-09-18 17:38:47 +0200

    some comments

--- tools/bin_to_ppm.c
+++ tools/bin_to_ppm.c
@@ -26,9 +26,11 @@ int convert(const char *source_path, const char *destination_path, long width, l
       printf("Could not open %s for reading\n", source_path);
       return EXIT_FAILURE;
   }
-  fprintf(destp, "P5\n%ld %ld\n255\n", width, height);
+  // Go to the offset
   fseek(sourcep, offset, SEEK_SET);
+  // Let's write this PGM file. First, the header
   printf("Writing %s\n", destination_path);
+  fprintf(destp, "P5\n%ld %ld\n255\n", width, height);
   for (j = 0; j < width; ++j)
   {
     for (i = 0; i < height; ++i)
@@ -56,6 +58,10 @@ int main(int argc, char **argv) {
   long inputfile_size = 0;
   long page_size = 0;
   int filesplit_number = 8;
+  int res = 0;
+
+  char *inputfile_path = argv[1];
+  char *destdir_path = argv[3];
 
   if (argc == 1 || (strncmp(argv[1], "--help", 6)==0) || (strncmp(argv[1], "-h", 6) == 0)) {
       return usage();
@@ -65,22 +71,20 @@ int main(int argc, char **argv) {
       return usage();
   }
 
-  char *inputfile_path = argv[1];
-  char *destdir_path = argv[3];
-
-  int res = stat(inputfile_path, &st);
+  res = stat(inputfile_path, &st);
   if (res != 0) {
       printf("ERROR: can't read input file size: '%s'\n", inputfile_path);
       return EXIT_FAILURE;
   }
-  inputfile_size = st.st_size;
 
+  // Some input file size sanity checks
+  inputfile_size = st.st_size;
   page_size = strtol(argv[2], NULL, 10);
   if ((page_size < 1) || (page_size > 1*1024*1024)) {
       printf("Invalid page size: '%s'\n", argv[2]);
       return EXIT_FAILURE;
   }
-
+  printf("%d\n",filesplit_number);
   if (inputfile_size % page_size != 0) {
       printf("WARNING: %s is of size %ld, which is not a multiple of page size %ld\n", inputfile_path, inputfile_size, page_size);
       printf("WARNING: We might be losing some data\n");
@@ -91,8 +95,8 @@ int main(int argc, char **argv) {
   }
 
   const int dimx = page_size;
-  const long pixel_by_pic = inputfile_size / filesplit_number;
-  const int dimy = pixel_by_pic / dimx;
+  const long pixels_by_pic = inputfile_size / filesplit_number;
+  const int dimy = pixels_by_pic / dimx;
 
   char *inputfile_name = basename(inputfile_path);
   printf("I'll generate %d PGM files for file %s\n", filesplit_number, inputfile_name);
@@ -100,7 +104,9 @@ int main(int argc, char **argv) {
   char *dest_picture_path = (char *)malloc(PATH_MAX);
   long offset = 0;
   for (int i=0; i < filesplit_number; i++) {
-      snprintf(dest_picture_path, PATH_MAX, "%s/%s_%d_%ld-%ld.pgm", destdir_path, inputfile_name, i, offset, offset+pixel_by_pic);
+      // the output PGM files are goint go look like
+      // dest_dir/dump.bin_0_start-end.pgm
+      snprintf(dest_picture_path, PATH_MAX, "%s/%s_%d_%ld-%ld.pgm", destdir_path, inputfile_name, i, offset, offset+pixels_by_pic);
       res = convert(inputfile_name, dest_picture_path, dimx, dimy, offset);
       if (res != 0 ) {
           printf("ERROR trying to convert %s\n", dest_picture_path);
@@ -108,7 +114,7 @@ int main(int argc, char **argv) {
           return res;
       }
 
-      offset += pixel_by_pic;
+      offset += pixels_by_pic;
   }
   free(dest_picture_path);
 
